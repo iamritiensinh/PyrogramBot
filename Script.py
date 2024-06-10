de256b16417372aa8c6bf9681b774ff1d1bedf5b
iamritiensinh <ritiensinh@icloud.com>
2024-06-10 17:47:14 +0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,22 +1,19 @@
 class Script:
     """
-    bot greeting texts and other texts
+    Văn bản chào hỏi bot và các văn bản khác
     """
     START_TXT = """
-Hello {}, I am [Pyrogram Bot](https://github.com/kalanakt/Pyrogram-Telegram-Bot-Template), a simple bot.
+Hello {}, I am [Pyrogram Bot](https://github.com/iamritiensinh/PyrogramBot), a simple bot.
 """
 
     HELP_TXT = """
-Add help message for users
+Thêm tin nhắn trợ giúp cho người dùng
 """
 
     ABOUT_TXT = """
-✯ My Name: [Pyrogram Bot](https://github.com/kalanakt/Pyrogram-Telegram-Bot-Template)
+✯ My Name: [Pyrogram Bot](https://github.com/iamritiensinh/PyrogramBot)
 ✯ Library: [Pyrogram v1.2.0](https://docs.pyrogram.org/)
-✯ Server: [Heroku](https://dashboard.heroku.com/)
 ✯ Language: [Python 3.9](https://docs.python.org/3/)
 ✯ Database: [MongoDB](https://mongodb.com/)
-✯ Base Source Code: [Pyrogram Telegram Bot Template](https://github.com/kalanakt/Pyrogram-Telegram-Bot-Template)
-✯ Update Channel: [Click Here](https://t.me/TMWAD)
-✯ Maintenance: [Hasnain](https://github.com/kalanakt)
+✯ Bảo trì: [Ri Tiên Sinh](https://github.com/iamthayrio)
 """
